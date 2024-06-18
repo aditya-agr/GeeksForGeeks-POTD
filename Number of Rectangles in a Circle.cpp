4de6fb31ac562c2af96cd450a3991d7e4074ceeb
Aditya Agrawal <agr2003aditya@gmail.com>
2024-06-18 13:58:47 +0530

    Solution of 18-06-2024

---
+++ Number of Rectangles in a Circle.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int rectanglesInCircle(int r) {
+        // code here
+        int cnt=0;
+        for (int i=1; i<=2*r; i++)
+            for(int j=1; j<=2*r; j++)
+                if(i*i + j*j <= 4*r*r)
+                    cnt++;
+        return cnt;
+    }
+};
