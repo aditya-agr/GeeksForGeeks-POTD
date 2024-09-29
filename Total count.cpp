431eddd7a0857caeb0d7e157d2d2101366f9667f
Aditya Agrawal <agr2003aditya@gmail.com>
2024-09-29 19:01:47 +0530

    Solution of 29-09-2024

---
+++ Total count.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int totalCount(int k, vector<int>& arr) {
+        // code here
+        int res = 0;
+        for(int x : arr){
+            res += x/k;
+            if(x%k)
+                res++;
+        }
+        return res;
+    }
+};
