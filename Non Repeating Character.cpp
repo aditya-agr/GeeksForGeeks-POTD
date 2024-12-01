38d3418906ffeb850423cf314151c805576a708b
Aditya Agrawal <agr2003aditya@gmail.com>
2024-12-01 19:57:25 +0530

    Solution of 01-12-2024

---
+++ Non Repeating Character.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    // Function to find the first non-repeating character in a string.
+    char nonRepeatingChar(string &s) {
+        // Your code here
+        unordered_map<char,int> mp;
+        for(char c : s)
+            mp[c]++;
+        for(char c : s)
+            if(mp[c] == 1) return c;
+        return '$';
+    }
+};
