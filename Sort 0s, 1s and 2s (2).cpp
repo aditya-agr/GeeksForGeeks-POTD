336807a345d33ed99145eb3af58773a120574035
Aditya Agrawal <agr2003aditya@gmail.com>
2024-12-05 21:50:13 +0530

    Solution of 05-12-2024

---
+++ Sort 0s, 1s and 2s (2).cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        int i=0, j=0, k=n-1;
+        while(j<=k){
+            if(arr[j] == 0)
+                swap(arr[i++], arr[j++]);
+            else if(arr[j] == 1)
+                j++;
+            else
+                swap(arr[j], arr[k--]);
+        }
+    }
+};
