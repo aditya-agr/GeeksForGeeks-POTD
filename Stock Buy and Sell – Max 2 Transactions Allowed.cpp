9a232aa9eb7672fcaca2863093bd69e42848017b
Aditya Agrawal <agr2003aditya@gmail.com>
2025-03-20 21:04:11 +0530

    Solution of 20-03-2025

--- Stock Buy and Sell – Max 2 Transactions Allowed.cpp
+++ Stock Buy and Sell – Max 2 Transactions Allowed.cpp
@@ -23,5 +23,23 @@ class Solution {
             }
         }
         return dp[0][2][1];
+
+
+        int n = prices.size();
+        vector<int> profit(n+1, 0);
+        int maxi = 0;
+        for(int i=n-1; i>=0; i--){
+            profit[i] = max(maxi - prices[i], profit[i+1]);
+            maxi = max(maxi, prices[i]);
+        }
+        int mini = INT_MAX;
+        for(int i=0; i<n; i++){
+            profit[i] = max(prices[i] - mini + profit[i+1], profit[i-1]) ;
+            mini = min(mini, prices[i]);
+        }
+        return profit[n-1];
+
     }
 };
+
+
