56dac848220015843c1fc8e36cd2871d2d8df2f7
Aditya Agrawal <agr2003aditya@gmail.com>
2025-09-04 19:18:38 +0530

    Solution of 04-09-2025

---
+++ Linked List Group Reverse (2).cpp
@@ -0,0 +1,30 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    Node *reverseKGroup(Node *head, int k) {
+        // code here
+        if(!head || !head->next || k==1)
+            return head;
+        int K = k;
+        Node *curr=head, *prev=NULL, *nxt;
+        while(K-- && curr){
+            nxt = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nxt;
+        }
+        head->next = reverseKGroup(curr, k);
+        return prev;
+    }
+};
