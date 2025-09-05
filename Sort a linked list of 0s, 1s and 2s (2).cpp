c2f74fcb176ecdaed320fabd6336cd38c2684cf0
Aditya Agrawal <agr2003aditya@gmail.com>
2025-09-05 14:28:39 +0530

    Solution of 05-09-2025

---
+++ Sort a linked list of 0s, 1s and 2s (2).cpp
@@ -0,0 +1,43 @@
+/* Node is defined as
+  class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    Node* segregate(Node* head) {
+        // code here
+        int cnt0=0, cnt1=0, cnt2=0;
+        Node* temp = head;
+        while(temp){
+            if(temp->data == 0)
+                cnt0++;
+            else if(temp->data == 1)
+                cnt1++;
+            else
+                cnt2++;
+            temp = temp->next;
+        }
+        temp = head;
+        while(cnt0--){
+            temp->data = 0;
+            temp = temp->next;
+        }
+        while(cnt1--){
+            temp->data = 1;
+            temp = temp->next;
+        }
+        while(cnt2--){
+            temp->data = 2;
+            temp = temp->next;
+        }
+        return head;
+    }
+};
