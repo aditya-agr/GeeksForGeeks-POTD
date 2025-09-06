50ce9e1ae18e3f3699dda94d357816ba6209e8df
Aditya Agrawal <agr2003aditya@gmail.com>
2025-09-06 22:01:22 +0530

    Solution of 06-09-2025

---
+++ Find length of Loop (3).cpp
@@ -0,0 +1,41 @@
+/*
+class Node {
+ public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int lengthOfLoop(Node *head) {
+        if (head == NULL || head->next == NULL) return 0;
+
+        Node *slow = head, *fast = head;
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {  // Loop detected
+                return countLoopLength(slow);
+            }
+        }
+        return 0; // No loop found
+    }
+
+  private:
+    int countLoopLength(Node *nodeInLoop) {
+        int count = 1;
+        Node *temp = nodeInLoop->next;
+
+        while (temp != nodeInLoop) {
+            count++;
+            temp = temp->next;
+        }
+        return count;
+    }
+};
