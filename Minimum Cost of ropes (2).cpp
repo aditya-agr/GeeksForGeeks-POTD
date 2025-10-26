d3e7a34eee95a4f5120c0395281852a60b872e2d
Aditya Agrawal <agr2003aditya@gmail.com>
2025-10-26 22:13:07 +0530

    Solution of 26-10-2025

---
+++ Minimum Cost of ropes (2).cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int minCost(vector<int>& arr) {
+        // code here
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(int x : arr)
+            pq.push(x);
+        
+        int tot = 0;
+        while(pq.size() > 1){
+            int a = pq.top();
+            pq.pop();
+            int b = pq.top();
+            pq.pop();
+            tot += a+b;
+            pq.push(a+b);
+        }
+        return tot;
+    }
+};
