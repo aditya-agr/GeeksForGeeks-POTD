056cf7b98ea78edbaa1505c4e67a84873d7600f1
Aditya Agrawal <agr2003aditya@gmail.com>
2026-02-05 15:06:00 +0530

    Solution of 05-02-2026

---
+++ Maximize Number of 1's (2).cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int maxOnes(vector<int>& arr, int k) {
+        // code here
+        int n = arr.size();
+        int i=0, j=0, cnt=0;
+        int res = 0;
+        while(j<n){
+            if(arr[j] == 0)
+                cnt++;
+            while(cnt > k){
+                if(arr[i] == 0)
+                    cnt--;
+                i++;
+            }
+            res = max(res, j-i+1);
+            j++;
+        }
+        return res;
+    }
+};
