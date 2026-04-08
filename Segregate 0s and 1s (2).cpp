58f0e030ff8788e0c805413a3145c79a985fda16
Aditya Agrawal <agr2003aditya@gmail.com>
2026-04-08 14:34:24 +0530

    Solution of 08-04-2026

---
+++ Segregate 0s and 1s (2).cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    void segregate0and1(vector<int> &arr) {
+        // code here
+        int i = 0;
+        int j = arr.size()-1;
+        while(i < j){
+            while(i<j && arr[i] == 0)
+                i++;
+            while(i<j && arr[j] == 1)
+                j--;
+            if(i < j)
+                swap(arr[i], arr[j]);
+        }
+    }
+};
