5c05de52d20cf6be5a99985e028b3e5140db864e
Aditya Agrawal <agr2003aditya@gmail.com>
2026-04-19 22:25:02 +0530

    Solution of 19-04-2026

---
+++ Check for Power.cpp
@@ -0,0 +1,8 @@
+class Solution {
+  public:
+    bool isPower(int x, int y) {
+        // code here
+        int _exp = round(1.00*log(1.00*y)/log(1.00*x));
+        return (pow(x, _exp) == y);
+    }
+};
