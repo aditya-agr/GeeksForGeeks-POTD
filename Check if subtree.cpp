0b8c8fe4362015aea5da24a2e69fbb25151d734a
Aditya Agrawal <agr2003aditya@gmail.com>
2026-05-07 19:45:57 +0530

    Solution of 07-05-2026

---
+++ Check if subtree.cpp
@@ -0,0 +1,36 @@
+/*
+Definition for Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    bool isSubTreeEqual(Node *root1, Node *root2){
+        if(root1 == NULL && root2 == NULL)
+            return true;
+        if(root1 == NULL || root2 == NULL || root1->data!=root2->data)
+            return false;
+        return isSubTreeEqual(root1->left, root2->left) && isSubTreeEqual(root1->right, root2->right);
+        
+    }
+    bool isSubTree(Node *root1, Node *root2) {
+        // code here
+        if(root1 == NULL && root2 == NULL)
+            return true;
+        if(root1 == NULL || root2 == NULL)
+            return false;
+        if(root1->data == root2->data && isSubTreeEqual(root1, root2))
+            return true;
+        return isSubTree(root1->left, root2) || isSubTree(root1->right, root2);
+    }
+};
