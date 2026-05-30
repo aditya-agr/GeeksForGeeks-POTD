47c550ffb3bfc3b7291f902069e94d6474cb2b94
Aditya Agrawal <agr2003aditya@gmail.com>
2026-05-30 23:04:48 +0530

    Solution of 30-05-2026

---
+++ Replace with XOR of Adjacent.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    void replaceElements(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        int prev = arr[0];
+        // Update first element
+        arr[0] = arr[0] ^ arr[1];
+
+        // Update middle elements
+        for (int i = 1; i < n - 1; i++) {
+            int curr = arr[i];
+            arr[i] = prev ^ arr[i + 1];
+            prev = curr;
+        }
+
+        // Update last element
+        arr[n - 1] = prev ^ arr[n - 1];
+    }
+};
