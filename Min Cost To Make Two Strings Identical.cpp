d5d066632b84a59653d2665d39352f0ced85230a
Aditya Agrawal <agr2003aditya@gmail.com>
2026-09-19 23:21:33 +0530

    Solution of 19-09-2026

---
+++ Min Cost To Make Two Strings Identical.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int findMinCost(string &s1, string &s2, int costS1, int costS2) {
+        // code here
+        int n = s1.size();
+        int m = s2.size();
+        
+        vector<vector<int>> dp(n+1, vector<int>(m+1));
+        for(int i=n-1; i>=0; i--){
+            for(int j=m-1; j>=0; j--){
+                if(s1[i]==s2[j])
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                else
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
+            }
+        }
+        int res = (n-dp[0][0])*costS1;
+        res += (m-dp[0][0])*costS2;
+        return res;
+    }
+};
